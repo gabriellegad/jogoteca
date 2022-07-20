7a95d41215a144c80fd15c54e9db74baaf814508
gabrielle gad <gabriellegad45@gmail.com>
2022-07-20 12:20:34 -0300

    jogotequinha

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+jogo1 = Jogo('Detroid Become Human', 'Ação e Aventura', 'Playstation')
+jogo2 = Jogo('Resident Evil Village', 'Survival horror ', 'Playstation')
+jogo3 = Jogo('uncharted the lost legacy', 'ação-aventura', 'Playstation')
+
+lista = [jogo1, jogo2, jogo3]
+
 app = Flask(__name__)
-
-lista = ['Detroid Become Human', 'Resident Evil Village', 'uncharted the lost legacy']
-
 @app.route('/inicio')
 def ola():
-    return render_template('lista.html', titulo='Meus Jogos', jogos = lista)
+    return render_template('lista.html', titulo='Meus Jogos', jogos=lista)
 
+@app.route('/novo')
+def novo():
+    return render_template('novo.html', titulo='Novo Jogo')
+    
 app.run()
